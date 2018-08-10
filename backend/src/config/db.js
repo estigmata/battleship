1d4fc6bebb3dde07cf92587f02135983137c595d
Nestor Otondo <nestor.otondo@fundacion-jala.org>
2018-08-10 00:49:04 -0400

    Remove commentaries

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -21,27 +21,3 @@ db.sequelize = sequelize
 db.game = require('../models/game')(sequelize, Sequelize)
 
 module.exports = db
-
-/*
-const Sequelize = require('sequelize')
-const GameModel = require('./models/game')
-const sequelize = new Sequelize('master', 'sa', 'sql.root.123', {
-  host: '10.28.106.76',
-  dialect: 'mssql',
-  port: 1433,
-  logging: false,
-  dialectOptions: {
-    requestTimeout: 30000,
-    encrypt: true
-  }
-})
-
-const Game = GameModel(sequelize, Sequelize)
-
-sequelize.sync({force: true})
-  .then(() => {
-    console.log('Database & tables created!');
-  })
-
-module.exports = { Game }
-*/
